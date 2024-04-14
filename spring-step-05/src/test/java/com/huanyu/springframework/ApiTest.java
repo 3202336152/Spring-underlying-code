f2edcde1ff9d535e61532b9ef9eeabbec0839dab
huanyu <3202336152@qq.com>
2024-04-14 13:48:49 +0800

    init

--- spring-step-05/src/test/java/com/huanyu/springframework/ApiTest.java
+++ spring-step-05/src/test/java/com/huanyu/springframework/ApiTest.java
@@ -1,16 +1,24 @@
 package com.huanyu.springframework;
 
+import cn.hutool.core.io.IoUtil;
 import com.huanyu.springframework.bean.UserDao;
 import com.huanyu.springframework.bean.UserService;
+import com.huanyu.springframework.core.io.DefaultResourceLoader;
+import com.huanyu.springframework.core.io.Resource;
 import com.huanyu.springframework.factory.config.BeanDefinition;
 import com.huanyu.springframework.factory.config.BeanReference;
 import com.huanyu.springframework.factory.support.DefaultListableBeanFactory;
+import com.huanyu.springframework.factory.xml.XmlBeanDefinitionReader;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * ClassName: ApiTest
  * Package: com.huanyu.springframework
- * Description:
+ * Description: 通过读取xml文件
  *
  * @Author: 寰宇
  * @Create: 2024/4/11 15:31
@@ -37,8 +45,60 @@ public class ApiTest {
 
         // 5. UserService 获取bean UserDao的信息是通过动态代理获取
         UserService userService = (UserService) beanFactory.getBean("userService");
-        userService.queryUserInfo();
+        String result = userService.queryUserInfo();
+        System.out.println("测试结果：" + result);
     }
+
+    private DefaultResourceLoader resourceLoader;
+
+    @Before
+    public void init() {
+        resourceLoader = new DefaultResourceLoader();
+    }
+
+    // 测试类文件加载
+    @Test
+    public void test_classpath() throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:important.properties");
+        InputStream inputStream = resource.getInputStream();
+        String content = IoUtil.readUtf8(inputStream);
+        System.out.println(content);
+    }
+
+    // 测试文件加载
+    @Test
+    public void test_file() throws IOException {
+        Resource resource = resourceLoader.getResource("src/test/resources/important.properties");
+        InputStream inputStream = resource.getInputStream();
+        String content = IoUtil.readUtf8(inputStream);
+        System.out.println(content);
+    }
+
+    @Test
+    public void test_url() throws IOException {
+        // 网络原因可能导致GitHub不能读取，可以放到自己的Gitee仓库。读取后可以从内容中搜索关键字；OLpj9823dZ
+        Resource resource = resourceLoader.getResource("https://github.com/fuzhengwei/small-spring/blob/main/important.properties");
+        InputStream inputStream = resource.getInputStream();
+        String content = IoUtil.readUtf8(inputStream);
+        System.out.println(content);
+    }
+
+    // 配置文件为入口解析和注册 Bean 信息
+    @Test
+    public void test_xml() {
+        // 1.初始化 BeanFactory
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+        // 2. 读取配置文件&注册Bean
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+        reader.loadBeanDefinitions("classpath:spring.xml");
+
+        // 3. 获取Bean对象调用方法
+        UserService userService = (UserService) beanFactory.getBean("userService", UserService.class);
+        String result = userService.queryUserInfo();
+        System.out.println("测试结果：" + result);
+    }
+
 
 
 }
--- spring-step-05/src/test/java/com/huanyu/springframework/bean/UserService.java
+++ spring-step-05/src/test/java/com/huanyu/springframework/bean/UserService.java
@@ -14,8 +14,8 @@ public class UserService {
 
     private UserDao userDao;
 
-    public void queryUserInfo() {
-        System.out.println("查询用户信息：" + userDao.queryUserName(uId));
+    public String queryUserInfo() {
+        return userDao.queryUserName(uId);
     }
 
     public String getuId() {
