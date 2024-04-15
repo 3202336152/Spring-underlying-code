679ba27aaf82ed096ba92f550ddf1e78a711653d
huanyu <3202336152@qq.com>
2024-04-15 14:39:03 +0800

    实现 Bean 的定义、注册、获取

--- spring-step-02/src/main/java/com/huanyu/springframework/BeanFactory.java
+++ spring-step-02/src/main/java/com/huanyu/springframework/BeanFactory.java
@@ -3,7 +3,7 @@ package com.huanyu.springframework;
 /**
  * ClassName: BeanFactory
  * Package: com.huanyu.springframework
- * Description: 管理和获取bean容器的根接口
+ * Description: 管理和获取bean容器的根接口。
  *
  * @Author: 寰宇
  * @Create: 2024/4/11 16:00
