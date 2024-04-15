5b9cff49803292e2f070dca0514e31377e285bcf
huanyu <3202336152@qq.com>
2024-04-15 14:28:48 +0800

    实现应用上下文，自动识别、资源加载、扩展机制

--- spring-step-06/src/main/java/com/huanyu/springframework/context/ApplicationContext.java
+++ spring-step-06/src/main/java/com/huanyu/springframework/context/ApplicationContext.java
@@ -6,7 +6,7 @@ import com.huanyu.springframework.factory.ListableBeanFactory;
 /**
  * ClassName: ApplicationContext
  * Package: com.huanyu.springframework.context
- * Description: 上下文接口，可以获取所有bean。
+ * Description: 应用上下文接口，可以获取所有bean。
  * 应用上下文负责创建、配置和管理Bean实例。
  * 它读取配置元数据（如XML配置文件、Java注解或Java配置类），根据配置信息创建Bean，并将它们组织成一个松散耦合的应用程序。
  * 应用上下文还负责解析Bean之间的依赖关系，并将依赖的Bean自动注入到相应的位置。
