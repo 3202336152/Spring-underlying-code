3c42049460500da930cc5b2af64ee74d832f939d
huanyu <3202336152@qq.com>
2024-04-15 14:40:01 +0800

    基于Cglib实现含构造函数的类实例化策略

--- spring-step-03/src/main/java/com/huanyu/springframework/factory/config/SingletonBeanRegistry.java
+++ spring-step-03/src/main/java/com/huanyu/springframework/factory/config/SingletonBeanRegistry.java
@@ -4,7 +4,8 @@ package com.huanyu.springframework.factory.config;
  * ClassName: SingletonBeanRegistry
  * Package: com.huanyu.springframework.config
  * Description:定义一个获取单例对象的接口。
- * 定义共享 bean 实例注册表的接口。 可以由 BeanFactory 实现来实现，以便以统一的方式公开其单例管理工具。
+ * 定义共享 bean 实例注册表的接口。
+ * 可以由 BeanFactory 实现来实现，以便以统一的方式公开其单例管理工具。
  * @Author: 寰宇
  * @Create: 2024/4/11 15:53
  * @Version: 1.0
